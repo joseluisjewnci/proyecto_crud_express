3feb86b9d0c3201b8683554e2893e1a0affc715e
joseluisjewnci <262038312+joseluisjewnci@users.noreply.github.com>
2026-08-24 11:14:13 -0500

    tercer commit module

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
-require('dotenv').config();
-const express = require('express');
+import 'dotenv/config';
+import express from 'express';
 const app = express();
+
 const mipuerto = process.env.MIPUERTO || 3003;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Servidor funcionando correctamente');
+});
 
 app.listen(mipuerto, () => {
     console.log(`Servidor ejecutándose en el puerto ${mipuerto}`);
