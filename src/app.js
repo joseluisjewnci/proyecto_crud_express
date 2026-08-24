aab2a1acd6117445286809a572ab05cbba500e99
joseluisjewnci <262038312+joseluisjewnci@users.noreply.github.com>
2026-08-24 11:07:29 -0500

    Primer commit configuracion inicial del proyecto

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
-import 'dotenv/config';
-
+require('dotenv').config();
+const express = require('express');
+const app = express();
 const mipuerto = process.env.MIPUERTO || 3003;
+
+app.listen(mipuerto, () => {
+    console.log(`Servidor ejecutándose en el puerto ${mipuerto}`);
+});
