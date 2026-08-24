c4da63997c5be06ba85414a148e959cbf82757c0
joseluisjewnci <262038312+joseluisjewnci@users.noreply.github.com>
2026-08-24 11:17:16 -0500

    cuarto commit api funcionando

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const mipuerto = process.env.MIPUERTO || 3003;
 
+app.get("/",(_,res)=>{
+    res.send('API Rest Full con express');
+});
+
 app.listen(mipuerto, () => {
     console.log(`Servidor ejecutándose en el puerto ${mipuerto}`);
 });
