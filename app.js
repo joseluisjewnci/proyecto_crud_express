97015c465e9912c0c276841c961ea6f3860b7df9
joseluisjewnci <262038312+joseluisjewnci@users.noreply.github.com>
2026-09-07 10:26:27 -0500

    añadir libreria multer

---
+++ app.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mipuerto = process.env.MIPUERTO || 3003; //middleware body-parse
+const sistemaArchivo = require("fs")
+const ruta = require("path")
+const rutaMiArchivo = ruta.join (__dirname, "datos.json")
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get("/",(_,res)=>{
+    res.send('API Rest Full con express');
+});
+
+app.get("/api/aprendices",(req,res)=>{
+    // res.status(200).json({mesagge: 'lista aprendices'});
+    sistemaArchivo.readFile(rutaMiArchivo , "utf-8", (error , datos )=>{
+        if (error) res.status(500).json({error : "no se puede leer el archivo"});
+        const listaAprendices = JSON.parse(datos)
+        res.status(200).json({Listado: listaAprendices})}
+    )
+});
+
+
+app.post("/api/aprendices",(req,res)=>{
+    const datosAprendiz = req.body
+    sistemaArchivo.readFile(rutaMiArchivo , "utf-8", (error , datos )=>{
+        if (error) res.status(500).json({error : "no se puede leer el archivo"});
+        const listaAprendices = JSON.parse(datos)
+        listaAprendices.push(datosAprendiz)
+    sistemaArchivo.writeFile(rutaMiArchivo, JSON.stringify(listaAprendices, null, 2), (error)=>{
+        if (error) res.status(500).json({Error: "no se puede escribir en le file"});
+        res.status(201).json({mensaje: "creado", Datos: datosAprendiz})}
+    )})
+});
+    
+
+app.put("/api/aprendices/:id",(req ,res)=>{
+    res.status(200).json({message: 'Actualizar aprendiz'})
+});
+
+app.delete("/api/aprendices/:id",(_,res)=>{
+    res.status(200).json({message: 'Eliminado'})
+});
+
+app.listen(mipuerto, () => {
+    console.log(`Servidor ejecutándose en el puerto ${mipuerto}`);
+});
